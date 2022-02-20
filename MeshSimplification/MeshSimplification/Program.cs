aad35999ab8f638dca41a4710c2c65e10adc61b4
Andrew <andrewkostin255@ya.ru>
2022-02-20 21:50:19 +0700

    add project for rider enjoyers

---
+++ MeshSimplification/MeshSimplification/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using MeshSimplification.Algorithms;
+using MeshSimplification.Readers.Exporter;
+using MeshSimplification.Readers.Importer;
+using MeshSimplification.Types;
+
+namespace MeshSimplification{
+    internal class Program{
+        public static void Main(string[] args){
+            Stopwatch stopWatch = new Stopwatch();
+            TimeSpan ts;
+            string elapsedTime;
+            string path = @"/home/andrey/Downloads/help/ascii/hind.ply";
+
+            EdgeContraction edgeContraction = new EdgeContraction();
+            FaceContraction faceContraction = new FaceContraction();
+            MyAlgorithm myAlgorithm = new MyAlgorithm();
+
+            rmUselessVertices rm = new rmUselessVertices();
+            
+            
+            ImporterPly importer = new ImporterPly();
+            ExporterPly exporterPly = new ExporterPly();
+            
+            Model figure = importer.Import(path);
+            
+            
+            
+            stopWatch.Start();
+
+            //Model simple = figure;
+            Model simple = edgeContraction.Simplify(figure, 0.1);
+            //Model simple = edgeContraction.Simplify(figure);
+            //Model simple = faceContraction.Simplify(figure, 0.02);
+            //Model simple = myAlgorithm.Simplify(figure, 0.56);
+            
+            stopWatch.Stop();
+
+            ts = stopWatch.Elapsed;
+            elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("Algorithm runtime: " + elapsedTime + "\n");
+            
+            
+            stopWatch.Restart();
+            rm.RemoveVertices(simple);
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("removing useless vertices runtime: " + elapsedTime + "\n");    
+            
+            stopWatch.Restart();
+
+            //exporterPly.Export(path, figure, false, false);
+            exporterPly.Export(path, simple, false, false);
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("Writing runtime: " + elapsedTime);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MeshSimplification/Tests/Tests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using MeshSimplification.Algorithms;
+using MeshSimplification.Types;
+using MeshSimplification.Readers.Importer;
+
+namespace Tests{
+    [TestFixture]
+    public class Tests{
+        string path = @"/home/andrey/Downloads/help/ascii/bunny.ply";
+        
+        [Test]
+        public void TestFaceContraction(){
+            FaceContraction faceContraction = new FaceContraction();
+            BoundBoxAABB boundBoxAABB = new BoundBoxAABB();
+            
+            ImporterPly importer = new ImporterPly();
+            Model figure = importer.Import(path);
+            
+            Model simple = faceContraction.Simplify(figure, 0.2);
+            Model box = boundBoxAABB.Simplify(figure);
+            Model boxSimple = boundBoxAABB.Simplify(simple);
+            
+            //check count of meshes
+            Assert.AreEqual(figure.Meshes.Count, simple.Meshes.Count);
+
+            //check volume
+            for (int i = 0; i < box.Meshes.Count; i++) {
+                for (int j = 0; j < box.Meshes[i].Vertices.Count; j++) { 
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].X, boxSimple.Meshes[i].Vertices[j].X);   
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].Y, boxSimple.Meshes[i].Vertices[j].Y);   
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].Z, boxSimple.Meshes[i].Vertices[j].Z);   
+                }                
+            } 
+            
+        }
+        
+        [Test]
+        public void TestEdgeContraction(){
+            EdgeContraction edgeContraction = new EdgeContraction();
+            BoundBoxAABB boundBoxAABB = new BoundBoxAABB();
+            
+            ImporterPly importer = new ImporterPly();
+            Model figure = importer.Import(path);
+            
+            Model simple = edgeContraction.Simplify(figure, 0.2);
+            Model box = boundBoxAABB.Simplify(figure);
+            Model boxSimple = boundBoxAABB.Simplify(simple);
+            
+            //check count of meshes
+            Assert.AreEqual(figure.Meshes.Count, simple.Meshes.Count);
+
+            //check volume
+            for (int i = 0; i < box.Meshes.Count; i++) {
+                for (int j = 0; j < box.Meshes[i].Vertices.Count; j++) { 
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].X, boxSimple.Meshes[i].Vertices[j].X, 0.0001);   
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].Y, boxSimple.Meshes[i].Vertices[j].Y, 0.0001);   
+                    Assert.AreEqual(box.Meshes[i].Vertices[j].Z, boxSimple.Meshes[i].Vertices[j].Z, 0.0001);   
+                }                
+            } 
+            
+        }
+    }
+}
